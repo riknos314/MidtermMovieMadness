defcfa34cda8216569c856d7cc6a305b14627e23
flysaiah <mayeis01@8255.dynamic.luther.edu>
2014-10-23 09:46:27 -0500

    Added API key for rotten tomatoes

--- movies.js
+++ movies.js
@@ -1,6 +1,6 @@
 getMovieInfo = function() {   //Gets Rotten Tomatoes info and puts it in a dictionary
 
-	var request = new XMLHttpRequest()
+	var request = new XMLHttpRequest();
 
     request.onreadystatechange = function() {
 
@@ -11,6 +11,16 @@ getMovieInfo = function() {   //Gets Rotten Tomatoes info and puts it in a dicti
         }
 	}
 
+	var movietitle = document.getElementbyId('movietitle'); //create query string
+	var wordlist = movietitle.split(" ");
+	var searchQuery = "";
+	for (var i=0; i < wordlist; i++) {
+		searchQuery = searchQuery + wordlist[i] + "+";
+	}
+	console.log(searchQuery); //for debugging just in case my method isn't quite right
+	
+	var targetURL
+	
 	
 
 }
