467bf3b8e4f84da5f6334f1ac86bc073dfb0e4e3
flysaiah <mayeis01@8255.dynamic.luther.edu>
2014-10-24 17:36:12 -0500

    Fixed searchQuery algorithm, added to DevelopmentNotes

--- movies.js
+++ movies.js
@@ -7,25 +7,22 @@ getMovieInfo = function() {   //Gets Rotten Tomatoes info
 	if (request.readyState == 4)
 		if (request.status == 200) {    //build dictionary in this block
 			
-			var infodict = JSON.parse(request.responseText);
-			
-			console.log(infodict.movies[0].synopsis);
+			var infodict = JSON.parse(request.responseText);     //this holds all the movie info			
+			var pgrating = infodict.movies[0].mpaa_rating;			
+			var criticrating = infodict.movies[0].ratings["critics_score"];			
+			var audiencerating = infodict.movies[0].ratings["audience_score"];
+			var synopsis = infodict.movies[0].synopsis;
+			var moviepic = infodict.movies[0].posters["thumbnail"]
         }
 	}
 
 	var movietitle = document.getElementById('movietitle'); //create query string
 	var wordlist = movietitle.value.split(" ");
-	var searchQuery = "";
-	if (wordlist.length <= 1) {        //FIX THIS
-		searchQuery = wordlist[0]
-	}
-	else {
-		for (var i=0; i < wordlist.length; i++) {
-			searchQuery = searchQuery + wordlist[i] + "+";
-			console.log(searchQuery);
+	var searchQuery = wordlist.shift();
+		while (wordlist.length != 0) {
+			searchQuery = searchQuery + "+" + wordlist.shift();
 		}
-	}
-
+	console.log(searchQuery);
 	
 	var targetURL = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=fxee5efruku5fre6wdbe7c6r&q=' + searchQuery + '&page_limit=1';
 		
